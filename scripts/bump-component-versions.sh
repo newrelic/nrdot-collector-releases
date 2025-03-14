c5e26db62a60a75dc70b577385b89551972ce430
kbauer <kbauer@newrelic.com>
2025-03-14 08:57:30 -0700

    chore: bump nrdot to 1.0.1

--- scripts/bump-component-versions.sh
+++ scripts/bump-component-versions.sh
@@ -66,29 +66,31 @@ escaped_current_beta_core=${current_beta_core//./\\.}
 escaped_current_beta_contrib=${current_beta_contrib//./\\.}
 escaped_current_stable=${current_stable//./\\.}
 
-# Determine the OS and set the sed command accordingly
+# Determine the OS and set the sed -i command accordingly
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  sed_command="sed -i ''"
+  function sed_inplace {
+  	sed -i '' "$@"
+  }
 else
-  # Linux
-  sed_command="sed -i"
+  function sed_inplace {
+    	sed -i'' "$@"
+  }
 fi
 
 # Update versions in each manifest file
 echo "Updating core beta version from $current_beta_core to $next_beta_core,"
 echo "core stable version from $current_stable to $next_stable,"
 echo "contrib beta version from $current_beta_contrib to $next_beta_contrib,"
-echo "and distribution version to $next_distribution_version"
 for file in ./distributions/*/manifest.yaml; do
   if [ -f "$file" ]; then
-    $sed_command "s/\(^.*go\.opentelemetry\.io\/collector\/.*\) v$escaped_current_beta_core/\1 v$next_beta_core/" "$file"
-    $sed_command "s/\(^.*github\.com\/open-telemetry\/opentelemetry-collector-contrib\/.*\) v$escaped_current_beta_contrib/\1 v$next_beta_contrib/" "$file"
-    $sed_command "s/\(^.*go\.opentelemetry\.io\/collector\/.*\) v$escaped_current_stable/\1 v$next_stable/" "$file"
+    sed_inplace "s/\(^.*go\.opentelemetry\.io\/collector\/.*\) v$escaped_current_beta_core/\1 v$next_beta_core/" "$file"
+    sed_inplace "s/\(^.*github\.com\/open-telemetry\/opentelemetry-collector-contrib\/.*\) v$escaped_current_beta_contrib/\1 v$next_beta_contrib/" "$file"
+    sed_inplace "s/\(^.*go\.opentelemetry\.io\/collector\/.*\) v$escaped_current_stable/\1 v$next_stable/" "$file"
   else
     echo "File $file does not exist"
   fi
 done
 
 # Update Makefile OCB version
-$sed_command "s/OTELCOL_BUILDER_VERSION ?= $escaped_current_beta_core/OTELCOL_BUILDER_VERSION ?= $next_beta_core/" Makefile
+sed_inplace "s/OTELCOL_BUILDER_VERSION ?= $escaped_current_beta_core/OTELCOL_BUILDER_VERSION ?= $next_beta_core/" Makefile
---
+++ scripts/bump-nrdot-version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Simple script to bump versions for new release to document all places that need to be updated
+set -e
+old_version=1.0.1
+new_version=1.0.2
+
+# Determine the OS and set the sed -i command accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  function sed_inplace {
+  	sed -i '' "$@"
+  }
+else
+  function sed_inplace {
+    	sed -i'' "$@"
+  }
+fi
+
+
+for manifest in ./distributions/*/manifest.yaml; do
+  if [ ! -f "${manifest}" ]; then
+    echo "File missing: ${manifest}"
+    exit 1
+  fi
+  current_version=$(yq ".dist.version" "${manifest}")
+  if [[ ${current_version} != "${old_version}" ]]; then
+    echo "Unexpected: Found ${current_version} instead of ${old_version} in ${manifest}"
+    exit 1
+  fi
+  sed_inplace "s/version: ${old_version}/version: ${new_version}/g" ${manifest}
+done
+
+if ! grep -q "${old_version}" ./distributions/README.md; then
+  echo "Didn't find old version in README.md - does the script need updating?"
+  exit 1
+else
+  sed_inplace "s/${old_version}/${new_version}/g" ./distributions/README.md
+fi
+
