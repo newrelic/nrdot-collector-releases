e231bfc15ff7391cba3d7f9f405a48b402470648
kb-newrelic <121687305+kb-newrelic@users.noreply.github.com>
2025-05-06 09:14:36 -0700

    chore: bump to 1.1.0 (#306)

--- scripts/bump-nrdot-version.sh
+++ scripts/bump-nrdot-version.sh
@@ -2,7 +2,9 @@
 # Simple script to bump versions for new release to document all places that need to be updated
 set -e
 old_version=1.0.3
-new_version=1.0.4
+new_version=1.1.0
+
+REPO_DIR="$( cd "$(dirname "$( dirname "${BASH_SOURCE[0]}" )")" &> /dev/null && pwd )"
 
 # Determine the OS and set the sed -i command accordingly
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -17,7 +19,7 @@ else
 fi
 
 
-for manifest in ./distributions/*/manifest.yaml; do
+for manifest in ${REPO_DIR}/distributions/*/manifest.yaml; do
   if [ ! -f "${manifest}" ]; then
     echo "File missing: ${manifest}"
     exit 1
@@ -30,10 +32,10 @@ for manifest in ./distributions/*/manifest.yaml; do
   sed_inplace "s/version: ${old_version}/version: ${new_version}/g" ${manifest}
 done
 
-if ! grep -q "${old_version}" ./distributions/README.md; then
+if ! grep -q "${old_version}" ${REPO_DIR}/distributions/README.md; then
   echo "Didn't find old version in README.md - does the script need updating?"
   exit 1
 else
-  sed_inplace "s/${old_version}/${new_version}/g" ./distributions/README.md
+  sed_inplace "s/${old_version}/${new_version}/g" ${REPO_DIR}/distributions/README.md
 fi
 
