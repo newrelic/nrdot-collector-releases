82079a4d4fbbde4836ea251191e28f3b385f50d4
Ansen Garvin <agarvin@newrelic.com>
2025-11-14 17:28:21 -0800

    chore: ensure build act-warp script works if certs directory doesn't exist

--- scripts/build-act-warp.sh
+++ scripts/build-act-warp.sh
@@ -1,9 +1,10 @@
-cd ./act/certs
-rm ./*
+set -e
+rm -rf ./act/certs && mkdir -p ./act/certs
+cd ./act/certs/
 security find-certificate -a -c "Gateway CA - Cloudflare Managed" -p | \
 awk '/-----BEGIN CERTIFICATE-----/{file="warp" ++i ".pem"} {print > file}'
 for pem in warp*.pem; do
   openssl x509 -in "$pem" -inform PEM -out "${pem%.pem}.crt"
+  rm "$pem"
 done
-rm ./*.pem
 docker build -t act-warp ../
