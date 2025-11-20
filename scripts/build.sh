d73d45385ea741c04346939394cb2ae0845a85f2
agarvin-nr <agarvin@newrelic.com>
2025-11-19 16:18:52 -0800

    chore: remove unnecessary explicit nrdot-plus skip in build script
    Co-authored-by: kb-newrelic <121687305+kb-newrelic@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -48,10 +48,6 @@ do
     build_folder="_build"
 
     if [[ "$fips" == true ]]; then
-      if [[ "$distribution" == "nrdot-collector-plus" ]]; then
-        echo "FIPS is not supported in NRDOT+. Skipping FIPS build."
-        continue
-      fi
       yq eval '
          .dist.name += "-fips" |
          .dist.description += "-fips" |
