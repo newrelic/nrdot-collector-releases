035db5eba2c1a8eb1e73ce2f45309ca7ee6cae88
kbauer <kbauer@newrelic.com>
2025-12-17 16:22:48 -0800

    ci: migrate nightly to e2e-action

--- test/terraform/nightly/vars.tf
+++ test/terraform/nightly/vars.tf
@@ -48,3 +48,15 @@ variable "image_tag" {
   description = "Tag of the nightly docker image"
   type        = string
 }
+
+variable "is_legacy_test" {
+  description = "Whether to deploy helm charts (enabled for Go tests, disabled for action tests)"
+  type        = bool
+  default     = false
+}
+
+variable "test_key" {
+  description = "Test key for scoping queries (used by action-based tests, Go tests use generated pattern)"
+  type        = string
+  default     = ""
+}
