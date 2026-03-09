cdc7af425fe6a95a0aed107cc829eb21b5a2e420
Mailo Arsac <marsac@newrelic.com>
2026-03-09 11:50:49 -0700

    chore: Bump to 1.11.1

--- scripts/bump-nrdot-version.sh
+++ scripts/bump-nrdot-version.sh
@@ -7,7 +7,7 @@ set -e
 
 REPO_DIR="$( cd "$(dirname "$( dirname "${BASH_SOURCE[0]}" )")" &> /dev/null && pwd )"
 old_version=$(yq ".dist.version" "${REPO_DIR}/distributions/nrdot-collector/manifest.yaml")
-new_version=1.11.0
+new_version=1.11.1
 
 
 # Determine the OS and set the sed -i command accordingly
