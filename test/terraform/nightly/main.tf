d70ddceb200f59f8daf8af9ab89fed4356948d4b
Ansen Garvin <agarvin@newrelic.com>
2026-05-11 15:43:53 -0700

    chore: invert module structure

--- test/terraform/nightly/main.tf
+++ test/terraform/nightly/main.tf
@@ -12,7 +12,7 @@ data "aws_eks_cluster" "eks_cluster" {
 
 module "ci_e2e_ec2" {
   count                = local.ec2_enabled ? 1 : 0
-  source               = "../modules/ec2"
+  source               = "../modules/ec2/ubuntu"
   test_environment     = local.test_env_name
   releases_bucket_name = local.releases_bucket_name
   collector_distro     = var.distro
